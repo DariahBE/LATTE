0d7c5e4a6898c0488d6245cf85326ccab552d39c
Frederic Pietowski <frederic_pietowski@hotmail.com>
2023-04-27 17:29:33 +0200

    export

--- includes/export.inc.php
+++ includes/export.inc.php
@@ -16,6 +16,7 @@ class Exporter {
   private $identified; 
   private $annotations; 
   private $breakpoints; 
+  private $XMLTaggedText;
   function __construct($client, $mode){
     $this->allowedModes = array('json', 'xml'); 
     $this->client = $client; 
@@ -37,14 +38,16 @@ class Exporter {
 
   public function setAnnotations($annotationArray){
     $this->annotations = $annotationArray; 
-    //$annotationArray['relations']['testOverlap'] = array('start'=> 1000, 'stop'=>1123);
+    $annotationArray['relations']['startTest'] = array('start'=> 0, 'stop'=>11);
+    $annotationArray['relations']['testOverlap'] = array('start'=> 1000, 'stop'=>1123);
+    $annotationArray['relations']['endTest'] = array('start'=> 1812, 'stop'=>1816);
     $breakpoints = array(); 
     $annotations = $annotationArray['relations']; 
     foreach($annotations as $key=> $value){
       for($i = $value['start']; $i <= $value['stop']; $i++){
         if(!(array_key_exists($i, $breakpoints))){
           $breakpoints[$i] = array(); 
-        }       
+        }
           $breakpoints[$i][] = $key; 
       }
     }
@@ -59,31 +62,107 @@ class Exporter {
     }
   }
 
+  public function outputContent(){
+    $date = date("d-m-Y H:i:s");
+    $exportURL = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']; 
+    $webURI = 'still working on this'; 
+    if($this->mode == 'xml'){
+      $dom = new DOMDocument();
+      $dom->encoding = 'utf-8';
+      $dom->xmlVersion = '1.0';
+      $dom->formatOutput = true;
+      //$xml_file_name = 'movies_list.xml'; 
+      $root = $dom->createElement('Export');
+      $metaNode = $dom->createElement('metadata');
+      $texNode = $dom->createElement('text'); 
+      $annoNode = $dom->createElement('annotation'); 
+      // assign fields to metadata: 
+      $metaTimeStamp = $dom->createElement('requestTime', $date); 
+      $metaSourceStamp = $dom->createElement( 'requestURI', htmlspecialchars($exportURL, ENT_XML1, 'UTF-8'));
+      $webView = $dom->createElement('stableURI', htmlspecialchars($webURI, ENT_XML1, 'UTF-8')); 
+      $metaNode->appendChild($metaTimeStamp); 
+      $metaNode->appendChild($metaSourceStamp); 
+      $metaNode->appendChild($webView); 
+      // assign data to text: 
+      $rawText = $dom->createElement('rawText', htmlspecialchars($this->rawtext)); 
+      $annotatedText = $dom->createElement('annotatedText', $this->XMLTaggedText);
+      $texNode->appendChild($rawText); 
+      $texNode->appendChild($annotatedText); 
+      //$attr_date_field = new DOMAttr('requestTime', $date); 
+      //$attr_movie_id = new DOMAttr('movie_id', '5467');
+      /*$movie_node->setAttributeNode($attr_date_field);
+      //$movie_node->setAttributeNode($attr_movie_id);
+      $child_node_title = $dom->createElement('Title', 'The Campaign');
+      $movie_node->appendChild($child_node_title);
+      $child_node_year = $dom->createElement('Year', 2012);
+      $movie_node->appendChild($child_node_year);
+      $child_node_genre = $dom->createElement('Genre', 'The Campaign');
+      $movie_node->appendChild($child_node_genre);
+      $child_node_ratings = $dom->createElement('Ratings', 6.2);
+      $movie_node->appendChild($child_node_ratings);*/
+      $root->appendChild($metaNode);
+      $root->appendChild($texNode);
+      $root->appendChild($annoNode);
+      $dom->appendChild($root);
+      //$dom->save($xml_file_name);
+      return $dom->saveXML(); //echo "$xml_file_name has been successfully created";
+  
+    }else if ($this->mode == 'json'){
+
+    }
+  }
+
   public function generateAnnotatedText(){
-    $prevAnnot = 0;
+    // BUG: export is not okay with this, you need to make a DOM object here!!
+    $prevAnnotKey = 0;
     $inAnnotationMode = False; 
     $annotatedText = ''; 
-    $oneAnnotation = ''; 
+    $oneAnnotation = '';
     foreach($this->identified as $key=> $value){
-      if($inAnnotationMode){
-
-      }
       if(!(array_key_exists($key, $this->breakpoints))){
-        $annotatedText.=$value;
-        $inAnnotationMode = False; 
-      }else{
-        //assign to prevAnnot: 
-        $inAnnotationMode = True; 
-        $prevAnnot = $value; 
-        $keyList = implode(',', $key); 
-        if (!($inAnnotationMode)){
-          $oneAnnotation .='<annotation id="'.$keyList.'">'; 
-          $inAnnotationMode = True;
+        //letter is not part of annotation and script was in annotationmode: close it off.
+        if ($inAnnotationMode){
+          $annotatedText .=$oneAnnotation.'</annotation>'; 
+          //reset to defaults!
+          $oneAnnotation = ''; 
+          $prevAnnotKey = 0; 
+          $inAnnotationMode = False; 
         }
+        // letter is not part of an annotation;
+        $annotatedText.=$value;
 
+      }else{
+        //letter is part of an annotation.
+        //Cases
+        //1) New annotation, 
+        //2) adding to one annotation.
+        //3) In annotationmode and creating new annotation!
+        $keyList = implode(', ', $this->breakpoints[$key]); 
+        $sameAsPrev = $keyList == $prevAnnotKey; 
+        //CASE1: 
+        if (!($inAnnotationMode)){
+          $oneAnnotation .='<annotation id="'.$keyList.'">'.$value;
+          $inAnnotationMode = True;
+
+          $prevAnnotKey = $keyList; 
+        }else{
+          $prevAnnotKey = $keyList; 
+          if ($sameAsPrev){
+            $oneAnnotation.=$value;
+          }else{
+            $oneAnnotation .= '</annotation>'; 
+            $annotatedText .=$oneAnnotation;
+            $oneAnnotation = '<annotation id="'.$keyList.'">'.$value;
+          }
+        }
       }
     }
-    return $annotatedText; 
+    if($oneAnnotation != ''){
+      //append trailing annotation: 
+      $oneAnnotation .= '</annotation>'; 
+      $annotatedText .=$oneAnnotation;
+    }
+    $this->XMLTaggedText =  $annotatedText; 
   }
 
   public function outputAnnotations(){
