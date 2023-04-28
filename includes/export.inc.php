2918dd1882923e2eb3631155d3b42bd0916f928f
Frederic Pietowski <frederic_pietowski@hotmail.com>
2023-04-28 12:57:37 +0200

    generateAnnotatedText has bug: char duplication

--- includes/export.inc.php
+++ includes/export.inc.php
@@ -40,6 +40,7 @@ class Exporter {
     $this->annotations = $annotationArray; 
     $annotationArray['relations']['startTest'] = array('start'=> 0, 'stop'=>11);
     $annotationArray['relations']['testOverlap'] = array('start'=> 1000, 'stop'=>1123);
+    $annotationArray['relations']['copyOverlap2'] = array('start'=> 1011, 'stop'=>1021);
     $annotationArray['relations']['endTest'] = array('start'=> 1812, 'stop'=>1816);
     $breakpoints = array(); 
     $annotations = $annotationArray['relations']; 
@@ -75,7 +76,7 @@ class Exporter {
       $root = $dom->createElement('Export');
       $metaNode = $dom->createElement('metadata');
       $texNode = $dom->createElement('text'); 
-      $annoNode = $dom->createElement('annotation'); 
+      $annoNode = $dom->createElement('annotatedText'); 
       // assign fields to metadata: 
       $metaTimeStamp = $dom->createElement('requestTime', $date); 
       $metaSourceStamp = $dom->createElement( 'requestURI', htmlspecialchars($exportURL, ENT_XML1, 'UTF-8'));
@@ -85,27 +86,23 @@ class Exporter {
       $metaNode->appendChild($webView); 
       // assign data to text: 
       $rawText = $dom->createElement('rawText', htmlspecialchars($this->rawtext)); 
-      $annotatedText = $dom->createElement('annotatedText', $this->XMLTaggedText);
       $texNode->appendChild($rawText); 
-      $texNode->appendChild($annotatedText); 
-      //$attr_date_field = new DOMAttr('requestTime', $date); 
-      //$attr_movie_id = new DOMAttr('movie_id', '5467');
-      /*$movie_node->setAttributeNode($attr_date_field);
-      //$movie_node->setAttributeNode($attr_movie_id);
-      $child_node_title = $dom->createElement('Title', 'The Campaign');
-      $movie_node->appendChild($child_node_title);
-      $child_node_year = $dom->createElement('Year', 2012);
-      $movie_node->appendChild($child_node_year);
-      $child_node_genre = $dom->createElement('Genre', 'The Campaign');
-      $movie_node->appendChild($child_node_genre);
-      $child_node_ratings = $dom->createElement('Ratings', 6.2);
-      $movie_node->appendChild($child_node_ratings);*/
+      foreach($this->XMLTaggedText as $key => $value){
+        $e = $this->XMLTaggedText[$key]; 
+        if ($e[0]=='Annotation'){
+          $elem = $dom->createElement('annotation', $e[1]);
+          $elemAtr = new DOMAttr('id', $key); 
+          $elem->setAttributeNode($elemAtr); 
+        }else{
+          $elem = $dom->createElement('unmarkedText', $e[1]);
+        }
+        $annoNode->appendChild($elem);
+      }
       $root->appendChild($metaNode);
       $root->appendChild($texNode);
       $root->appendChild($annoNode);
       $dom->appendChild($root);
-      //$dom->save($xml_file_name);
-      return $dom->saveXML(); //echo "$xml_file_name has been successfully created";
+      return $dom->saveXML(); 
   
     }else if ($this->mode == 'json'){
 
@@ -113,56 +110,42 @@ class Exporter {
   }
 
   public function generateAnnotatedText(){
-    // BUG: export is not okay with this, you need to make a DOM object here!!
     $prevAnnotKey = 0;
     $inAnnotationMode = False; 
     $annotatedText = ''; 
     $oneAnnotation = '';
+    $baseString = ''; 
+    $xml = new DOMDocument('1.0', 'iso-8859-1');
+    $this->XMLTaggedText = []; 
+    $rawTexkey = 0; 
+    $prevmode = False; 
+    $prevkeyList = False; 
+    $keyList = False;
+    //failure to detect overlapping annoations; text is duplicated!!
     foreach($this->identified as $key=> $value){
-      if(!(array_key_exists($key, $this->breakpoints))){
-        //letter is not part of annotation and script was in annotationmode: close it off.
-        if ($inAnnotationMode){
-          $annotatedText .=$oneAnnotation.'</annotation>'; 
-          //reset to defaults!
-          $oneAnnotation = ''; 
-          $prevAnnotKey = 0; 
-          $inAnnotationMode = False; 
+      if(array_key_exists($key, $this->breakpoints)){
+        $mode = 'annotation';
+        $keyList = implode(',', $this->breakpoints[$key]); 
+        if (!(array_key_exists($keyList, $this->XMLTaggedText))){
+          $this->XMLTaggedText[$keyList] = array('Annotation', ''); 
         }
-        // letter is not part of an annotation;
-        $annotatedText.=$value;
-
+        $this->XMLTaggedText[$keyList][1].=$value;
       }else{
-        //letter is part of an annotation.
-        //Cases
-        //1) New annotation, 
-        //2) adding to one annotation.
-        //3) In annotationmode and creating new annotation!
-        $keyList = implode(', ', $this->breakpoints[$key]); 
-        $sameAsPrev = $keyList == $prevAnnotKey; 
-        //CASE1: 
-        if (!($inAnnotationMode)){
-          $oneAnnotation .='<annotation id="'.$keyList.'">'.$value;
-          $inAnnotationMode = True;
-
-          $prevAnnotKey = $keyList; 
-        }else{
-          $prevAnnotKey = $keyList; 
-          if ($sameAsPrev){
-            $oneAnnotation.=$value;
-          }else{
-            $oneAnnotation .= '</annotation>'; 
-            $annotatedText .=$oneAnnotation;
-            $oneAnnotation = '<annotation id="'.$keyList.'">'.$value;
-          }
+        $keyList = 'textBlock_'.$rawTexkey; 
+        $mode = 'text'; 
+        
+        if ($prevmode != 'text'){
+          //echo "switch";
+          $rawTexkey =$rawTexkey+1; 
+          $keyList = 'textBlock_'.$rawTexkey; 
+          $this->XMLTaggedText[$keyList] = array('Textblock', ''); 
         }
+        //$this->XMLTaggedText[$keyList] = array('Annotation', ''); 
+        $this->XMLTaggedText[$keyList][1].=$value;
+
       }
+      $prevmode = $mode;
     }
-    if($oneAnnotation != ''){
-      //append trailing annotation: 
-      $oneAnnotation .= '</annotation>'; 
-      $annotatedText .=$oneAnnotation;
-    }
-    $this->XMLTaggedText =  $annotatedText; 
   }
 
   public function outputAnnotations(){
