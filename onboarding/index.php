a29e89f19f2e8f3d4788b413c1f5afddc566a7cf
Frederic Pietowski <frederic_pietowski@hotmail.com>
2023-10-23 16:11:39 +0200

    Started with onboarding folder. YOU WILL NEED TO MOVE LOGIN INFORMATION TO A PROTECTED .DB MYSQLITE FILE, CREATE THAT DURING THE ONBOARDING PROCESS!!

---
+++ onboarding/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="/JS/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="/CSS/style_entities.css">
+    <link rel="stylesheet" href="/CSS/stylePublic.css">
+    <link rel="stylesheet" href="/CSS/overlaystyling.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+
+<?php
+	
+
+
+
+
+
+?>
+
+<div>
+	<div class="p-2 text-4xl">Welcome!</div>
+
+	<div class="p-2">This will start the onboarding process; it is essential to complete this process before fully deploying the service.</div>
+
+	<div class="p-2 text-base">After completing the process, remove the 
+	<span class="font-bold">/onboarding/</span> folder from the root directory of the project.&nbsp;
+	<br>
+				
+	<br>This process will check some basic features to determine if the server is capable of hosting this tool; it is 
+	<span class="font-bold">not </span>a full-fledged configuration aid. For this, please refer to the accompanying documentation.
+	</div>
+
+	<div class="p-2"></div>
+</div>
+</body>
+</html>
