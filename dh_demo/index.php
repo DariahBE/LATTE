5c3978be1162e0fb8fdc24e06d80172683e98892
Frederic Pietowski <frederic_pietowski@hotmail.com>
2024-06-03 14:49:42 +0200

    cleanup

--- dh_demo/index.php
+++ dh_demo/index.php
@@ -11,46 +11,47 @@ $node->startTransaction();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+// Connect to SQLite database
+$pdo = new PDO('sqlite:sampledatabase.db');
+
+// Set error mode to exceptions
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// Fetch all records from the table
+$stmt = $pdo->query('SELECT * FROM exampledata'); 
+
+// Display records in an HTML table
+
+
+$i=0;
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $article = $row['article'];
+    $title = $row['title'];
+    $newspaper = $row['newspaper'];
+    $date = $row['date'];
+    $language = $row['language'];
+    $url = $row['digitized url']; 
+    $data = array(
+        'text'=>$article, 
+        'title'=>$title,
+        'publication'=>$newspaper, 
+        'date'=>$date,
+        'language'=>$language, 
+        'url'=>$url, 
+        'texid'=>$i++
+    ); 
+    
+  
+    $node->createNewNode("Text", $data, true);
+}
 try {
-    // Connect to SQLite database
-    $pdo = new PDO('sqlite:sampledatabase.db');
-
-    // Set error mode to exceptions
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Fetch all records from the table
-    $stmt = $pdo->query('SELECT * FROM exampledata'); 
-
-    // Display records in an HTML table
-
-
-    $i=0;
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $article = $row['article'];
-        $title = $row['title'];
-        $newspaper = $row['newspaper'];
-        $date = $row['date'];
-        $language = $row['language'];
-        $url = $row['digitized url']; 
-        $data = array(
-            'text'=>$article, 
-            'title'=>$title,
-            'publication'=>$newspaper, 
-            'date'=>$date,
-            'language'=>$language, 
-            'url'=>$url, 
-            'texid'=>$i++
-        ); 
-        
-      
-        $node->createNewNode("Text", $data, true);
-    }
 
     $node->commitTransaction();
+
+    
 } catch (PDOException $e) {
+    $node->rollbackTransaction();
     echo "Error: " . $e->getMessage();
 }
-
-$node->rollbackTransaction();
 
 ?>
